a0d75baa9493bbc8dc74789b7727962edca5d1fe
Camposmansi <Camposmansi@gmail.com>
2022-09-29 16:46:39 +0200

    Ejemplo2

---
+++ src/Ejemplos/Ejemplo3.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Ejemplo3 {
+    public static void main(String[] args) throws IOException {
+
+        File miDir = new File(".");
+        System.out.println("Directorio actual: " + miDir.getCanonicalPath());
+
+        //creamos objeto File al directorio donde esta Ejemplo2
+        File directorio = new File(".\\bin");
+
+        //El proceso a ejecutar es Ejemplo2
+        ProcessBuilder pb = new ProcessBuilder("java", "test2b.Ejemplo2");
+
+        //se establece el directorio donde se encuentra el ejecutable
+        pb.directory(directorio);
+
+        System.out.printf("Directorio de trabajo: %s%n", pb.directory());
+
+        //se ejecuta el proceso
+        Process p = pb.start();
+
+        //obtener la salida devuelta por el proceso
+        try {
+            InputStream is = p.getInputStream();
+            int c;
+            while ((c = is.read()) != -1)
+                System.out.print((char) c);
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            InputStream er = p.getErrorStream();
+            BufferedReader brer = new BufferedReader(new InputStreamReader(er));
+            String liner = null;
+            while ((liner = brer.readLine()) != null) System.out.println("Error >" + liner);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+    }
+}// Ejemplo3
